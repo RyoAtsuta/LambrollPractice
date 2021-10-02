32c9e6c081343f46ce2eabcb1b81701be152da52
熱田亮 <atsutaryou@atsutaryous-MacBook-puro.local>
2021-10-02 16:53:25 +0900

    update: change comment

--- app/test.py
+++ app/test.py
@@ -4,5 +4,5 @@ def execute(event, context):
     print("===== execute ====")
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!!!')
+        'body': json.dumps('Hello from Lambda2!!!')
     }
